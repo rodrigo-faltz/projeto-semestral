92a417036e34e1f40e93d8bf3ea46cfdd6b12380
acramin <132026903+acramin@users.noreply.github.com>
2024-10-11 10:30:11 -0300

    Update LocaleDate.java

--- src/LocaleDate.java
+++ src/LocaleDate.java
@@ -8,7 +8,7 @@ public class LocaleDate {
 
     private Calendar c = Calendar.getInstance();
     private Date data = c.getTime();
-    
+
     public void printDate(Locale locale) throws ParseException {
         DateFormat sdf = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.SHORT, locale);
         System.out.println(sdf.format(data));
